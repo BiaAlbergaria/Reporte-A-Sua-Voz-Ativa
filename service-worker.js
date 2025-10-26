66770a3a96ccfe36d47ca10a7e0009cd36a76aff
BiaAlbergaria <bbandeira005@gmail.com>
2025-10-25 21:52:02 -0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,15 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker instalado.');
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("reporte-cache").then(cache => {
+      return cache.addAll(["/"]);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
